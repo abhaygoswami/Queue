f891f1b9fe84c1001ad9c890a6f6153755cdcabd
BoRiNG-CoDE <abhaygoswami79265@gmail.com>
2017-11-14 14:07:49 +0530

    first commit

---
+++ Queue_Array.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#define max 10
+int queue[max];
+int front=-1,rear=-1;
+
+void add(void);
+void delete(void);
+void display(void);
+
+int main()
+{
+  int option,val;
+
+  do {
+    printf("\nPress 1 for ADD: ");
+    printf("\nPress 2 for DELETE: ");
+    printf("\nPress 3 for DISPLAY: ");
+    printf("\nPress 4 for QUIT: ");
+    scanf("\n%d",&option);
+
+    switch (option) {
+      case 1:add();
+      break;
+      case 2: delete();
+      break;
+      case 3: display();
+      break;
+    }
+  } while(option!=4);
+  return 0;
+}
+
+//ADD ELemenet is QUEUE
+
+void add()
+{
+  int n;
+  printf("\nEnter The Number: ");
+  scanf("%d",&n);
+
+  if(rear==max-1)
+  printf("\nStack is Overflow");
+  else if(front==-1&&rear==-1)
+  front=rear=0;
+  else
+  {
+  rear++;
+  queue[rear]=n;
+}
+}
+
+//Delete Element
+
+void delete()
+{
+  if(front==-1||front>rear)
+  printf("\nUnderflow");
+  else
+  {
+    queue[front]=NULL;
+    front++;
+  }
+}
+
+void display()
+{
+  if(front==-1||front>rear)
+  printf("\nUnderflow");
+  else
+  {
+    for(int i=front;i<=rear;i++)
+    printf("\n%d",queue[i] );
+  }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+			<BoRiNG_CoDE>
+			@A&A TEch
+			
+	This Repository @Queue contains Queue program implemented in C and Tested on MinGW GCC 
+		Compiler.
+	
+	Code is maintained By Abhay Goswami@abhaygoswami676@gmail.com(abhay676)
+			@A_A TEch
+				<BoRiNG_CoDE>
